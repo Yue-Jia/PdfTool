eb3b6be782bb3c4e10dc80f48a5ad969b541e498
Yue Jia <yjia@evertz.com>
2021-07-22 20:25:44 -0400

    add installLib

--- PdfTool.py
+++ PdfTool.py
@@ -1,5 +1,6 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader
 import sys
+import asyncio
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QProgressBar
 from PyQt5.QtCore import Qt, QUrl
 
@@ -47,13 +48,24 @@ class PdfTool(QMainWindow):
 		self.btn.clicked.connect(lambda : PT().pdfMerge(self.getItems()))
 		self.btn1 = QPushButton('Delete' ,self)
 		self.btn1.setGeometry(800,460,200,50)
-		self.btn1.clicked.connect(lambda :self.lstView.takeItem(self.getItems().index(self.getSelectedItem())))
-
+		self.btn1.clicked.connect(lambda :self.removeItem())
+		
 		self.setWindowTitle('PdfTool')
-		self.pbar = QProgressBar(self)
+		self.pbar = QProgressBar(self,minimum=0,maximum=0, textVisible=False,objectName='BlueProgressBar')
 		self.pbar.setRange(0,100)
-		self.pbar.setValue(65)
-		self.pbar.setGeometry(800,350,235,9)
+		self.pbar.setValue(100)
+		self.pbar.setGeometry(800,350,200,15)
+		self.pbar.setStyleSheet('''
+			#BlueProgressBar  {
+                        	border: solid grey;
+                          	border-radius: 15px;
+                          	color: black;
+                        }
+			#BlueProgressBar::chunk {
+				background-color: #05B8CC;
+                          	border-radius :15px;
+                          	}''')
+		self.setStyleSheet('background-color: lightgray')
 	def getSelectedItem(self):
         	item = QListWidgetItem(self.lstView.currentItem())
         	return item.text()
@@ -62,6 +74,11 @@ class PdfTool(QMainWindow):
         	for i in range(self.lstView.count()):
             		itt.append(self.lstView.item(i).text())
         	return itt
+	def removeItem(self):
+		try:	
+			self.lstView.takeItem(self.getItems().index(self.getSelectedItem()))
+		except Exception as e:
+			print(e)
 
 
 class PT():
@@ -70,16 +87,20 @@ class PT():
         	warnings.simplefilter("ignore")
 
 	def pdfMerge(self,lst):
-        	# Call the PdfFileMerger
-        	mergedObject = PdfFileMerger()
-        	print("Processing...") 
-        # 	Loop through all of pdf files and append their pages
-        	for fileName in lst:
-            		mergedObject.append(PdfFileReader(fileName, 'rb'))
-     
-        # 	Write all the files into a file which is named as shown below
-        	mergedObject.write("mergedfilesoutput.pdf")
-        	print("Done.")
+		try:
+			# Call the PdfFileMerger
+			mergedObject = PdfFileMerger()
+			print("Processing...") 
+		# 	Loop through all of pdf files and append their pages
+			for fileName in lst:
+				mergedObject.append(PdfFileReader(fileName, 'rb'))
+	     
+		# 	Write all the files into a file which is named as shown below
+			mergedObject.write("mergedfilesoutput.pdf")
+			print("Done.")
+		except Exception as e:
+			print(e)
+
 if __name__ =="__main__":
     
 #    try:
---
+++ libInstall.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PyPDF2'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PyQt5'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
