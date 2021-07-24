f5742567e5cf0b222efa6f2101d965c8c5c76753
Yue Jia <yjia@evertz.com>
2021-07-24 18:18:31 -0400

    add empty check

--- PdfTool.py
+++ PdfTool.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader
 import sys
 import asyncio
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QProgressBar, QFileDialog, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QProgressBar, QFileDialog, QLabel, QLineEdit
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt, QUrl
 
@@ -50,27 +50,19 @@ class PdfTool(QMainWindow):
 		self.setFixedSize(900,600)
 		self.lstView = ListBoxWidget(self)
 		self.lstView.setGeometry(300,0,600,600)
+		self.txt = QLineEdit(self)
+		self.txt.setGeometry(50,140,200,50)
+
 		self.btn = QPushButton('Merge' ,self)
 		self.btn.setGeometry(50,400,200,50)
 		self.btn.clicked.connect(lambda : self.pdfMerge(self.getItems(),self.msg()))
 		self.btn1 = QPushButton('Delete' ,self)
 		self.btn1.setGeometry(50,460,200,50)
 		self.btn1.clicked.connect(lambda :self.removeItem())
+		self.btn2 = QPushButton('Split',self)
+		self.btn2.setGeometry(50,200,200,50)
+
 		self.setWindowTitle('PdfTool')
-		self.pbar = QProgressBar(self,minimum=0,maximum=0, textVisible=False,objectName='BlueProgressBar')
-		self.pbar.setRange(0,100)
-		self.pbar.setValue(100)
-		self.pbar.setGeometry(50,350,200,15)
-		self.pbar.setStyleSheet('''
-			#BlueProgressBar  {
-                        	border: solid grey;
-                          	border-radius: 15px;
-                          	color: black;
-                        }
-			#BlueProgressBar::chunk {
-				background-color: #00FA9A;
-                          	border-radius :15px;
-                          	}''')
 		self.setStyleSheet('''
 				QPushButton {
 				    padding: 5px;
@@ -83,8 +75,8 @@ class PdfTool(QMainWindow):
 				background-color: #778899;
 				''')
 		self.setWindowIcon(QtGui.QIcon('PT.png'))
-		self.notif = QLabel('Please Drag and Drop',self)
-		self.notif.setGeometry(50,250,200,20)
+		self.notif = QLabel('',self)
+		self.notif.setGeometry(50,300,200,20)
 
 	def getSelectedItem(self):
         	item = QListWidgetItem(self.lstView.currentItem())
@@ -95,8 +87,11 @@ class PdfTool(QMainWindow):
             		itt.append(self.lstView.item(i).text())
         	return itt
 	def removeItem(self):
-		try:	
-			self.lstView.takeItem(self.getItems().index(self.getSelectedItem()))
+		try:
+			if self.getSelectedItem():
+				self.lstView.takeItem(self.getItems().index(self.getSelectedItem()))
+			else:
+				self.lstView.clear()
 		except Exception as e:
 			print(e)
 	def msg(self):
@@ -113,16 +108,22 @@ class PdfTool(QMainWindow):
 		try:
 			# Call the PdfFileMerger
 			mergedObject = PdfFileMerger()
-			print("Processing...") 
-		# 	Loop through all of pdf files and append their pages
-			for fileName in lst:
-				mergedObject.append(PdfFileReader(fileName, 'rb'))
-	     
-		# 	Write all the files into a file which is named as shown below
-			mergedObject.write(directory)
-			print("Done.")
+			if lst:
+				print("Processing...")
+				self.notif.setText('Processing...')
+			# 	Loop through all of pdf files and append their pages
+				for fileName in lst:
+					mergedObject.append(PdfFileReader(fileName, 'rb'))
+				
+			# 	Write all the files into a file which is named as shown below
+				mergedObject.write(directory)
+				print("Done.")
+				self.notif.setText('Done.')
+			else:
+				self.notif.setText('Select a file first')
 		except Exception as e:
 			print(e)
+			self.notif.setText(e)
 
 
 if __name__ =="__main__":
